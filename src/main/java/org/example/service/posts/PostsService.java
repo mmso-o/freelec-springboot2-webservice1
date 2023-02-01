ab2abcdbabf7b816aaf8e09e16c8eaf50f3e20bf
mmso-o <4.mmsoo@gmail.com>
2023-02-02 00:05:11 +0900

    등록 API 만들기

---
+++ src/main/java/org/example/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package org.example.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.posts.PostsRepository;
+import org.example.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
