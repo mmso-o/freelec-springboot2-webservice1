6dd4f6f095e897b7e0f0e871cd66b35c66f6eaa7
mmso-o <4.mmsoo@gmail.com>
2023-02-16 22:50:07 +0900

    feat: 게시글 수정 기능 구현

--- src/main/java/org/example/web/IndexController.java
+++ src/main/java/org/example/web/IndexController.java
@@ -2,16 +2,20 @@ package org.example.web;
 
 import lombok.RequiredArgsConstructor;
 import org.example.service.posts.PostsService;
+import org.example.web.dto.PostsResponseDto;
+import org.h2.engine.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
     @GetMapping("/")
     public String index(Model model){
-        model.addAttribute("posts", postsService.findAllDesc());
+        model.addAttribute("posts", postsService.findAllDesc()); // 서버 템플릿 엔진에서 사용할 수 있는 객체를 posts로 저장하여 index.mustache 에 전달한다.
 
         /*
         mustache starter 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨.
@@ -23,5 +27,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
